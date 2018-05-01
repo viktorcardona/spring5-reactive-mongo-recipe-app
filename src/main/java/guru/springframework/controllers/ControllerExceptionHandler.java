fa4c7c65fe969b66cf78bdb35d29d55f7261cd6a
Victor Cardona <victorcardona@Victors-MacBook-Air.local>
2018-05-01 10:40:58 -0500

    Handling Error NumberFormatException

--- src/main/java/guru/springframework/controllers/ControllerExceptionHandler.java
+++ src/main/java/guru/springframework/controllers/ControllerExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import org.thymeleaf.exceptions.TemplateInputException;
 
 
@@ -14,20 +15,17 @@ import org.thymeleaf.exceptions.TemplateInputException;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(NumberFormatException.class)
-//    public ModelAndView handleNumberFormat(Exception exception){
-//
-//        log.error("Handling Number Format Exception");
-//        log.error(exception.getMessage());
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//
-//        modelAndView.setViewName("400error");
-//        modelAndView.addObject("exception", exception);
-//
-//        return modelAndView;
-//    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WebExchangeBindException.class)
+    public String handleNumberFormat(Exception exception, Model model){
+
+        log.error("Handling Binding Exception");
+        log.error(exception.getMessage());
+
+        model.addAttribute("exception", exception);
+
+        return "400error";
+    }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({NotFoundException.class, TemplateInputException.class})
